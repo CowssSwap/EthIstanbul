eeb021dce19fa7d7566bb95bbff0a88897a7c352
NedenSinir <123alperozyurt@gmail.com>
2023-11-17 20:59:58 +0100

    wagmi chakra implement

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import WagmiProvider from '@/context/WagmiProvider'
+import { UIProvider } from '@/context/UIProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <WagmiProvider>
+        <UIProvider>
       <body className={inter.className}>{children}</body>
+      </UIProvider>
       </WagmiProvider>
       
     </html>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,27 @@
 "use client"
+import { Center, HStack, VStack, Text, Button, Heading } from '@chakra-ui/react'
 import Image from 'next/image'
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
-
 export default function Home() {
-  const { address, isConnected } = useAccount();
+  const { address, isConnected } = useAccount()
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   })
   const { disconnect } = useDisconnect()
  
-  if (isConnected)
-    return (
-      <div>
-        Connected to {address}
-        <button onClick={() => disconnect()}>Disconnect</button>
-      </div>
-    )
-  return <button onClick={() => connect()}>Connect Wallet</button>
+  return (
+    <Center mt={"5rem"}>
+      <VStack>
+        {isConnected ? (
+          <>
+            <Heading size={"xl"}>Connected to {address}</Heading>
+            <Button   outlineColor={"black"}onClick={() => disconnect()}>Disconnect</Button>
+          </>
+        ) : (
+          <Button outlineColor={"black"}  onClick={() => connect()}>Connect Wallet</Button>
+        )}
+      </VStack>
+    </Center>
+  )
 }
